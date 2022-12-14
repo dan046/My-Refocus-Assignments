6458d5d8607798a0c57cb65c3260ba8d6223eef5
IchirouTakashima <ichiroutakashimakun@gmail.com>
2022-12-14 18:47:46 +0800

    Manual Testing for BMI Calculator

--- Home Assignment. Git & GitHub/bmi_calculator.js
+++ Home Assignment. Git & GitHub/bmi_calculator.js
@@ -1,19 +1,38 @@
 // A simple JS code that calculates the person's BMI or Body Mass Index.
 
-// The formula for BMI is weight in kilograms divided by height in meters squared. If height has been measured in centimeters, divide by 100 to convert this to meters.
+// First, ask the user for their height in meters and their weight in kilograms.
+// Then, calculate the BMI using the formula: BMI = weight (kg) / height^2 (m^2).
+// Next, interpret the BMI value according to the following categories:
+// Under 18.5: Underweight
+// 18.5-24.9: Normal weight
+// 25.0-29.9: Overweight
+// 30.0 or higher: Obese
+
 function bmiCalculator(name, weight, height) {
   const bmi = weight / Math.pow(height, 2)
   console.log(
-    `All stats doesn't accurately says your health status. i.e. A muscular man can be called overweight.`
+    `DISCLAIMER: Caution should be observed when using the BMI as a measure for interpreting overweight and obesity as body composition can be highly variable yet have the same BMI. Therefore, BMI is not a reliable measurement of body composition in individuals particularly in older and younger people.`
   )
-  console.log(`Hi ${name}, your BMI is ${Math.round(bmi)}.`)
-  if (bmi > 18.5) {
-    console.log(`Weight Status: Healthy Weight.`)
-  } else if (bmi > 24.9) {
-    console.log(`Weight Status: Overweight.`)
-  } else if (bmi > 30) {
-    console.log(`Weight Status: Obese.`)
-  } else console.log(`Weight Status: Underweight.`)
+  console.log(`Hi ${name}!`)
+
+  if (bmi < 18.5) {
+    console.log(
+      `Your BMI is ${Math.round(bmi)}, which means you are underweight.`
+    )
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    console.log(
+      `Your BMI is ${Math.round(bmi)}, which means you are normal weight.`
+    )
+  } else if (bmi >= 24.9 && bmi < 30) {
+    console.log(
+      `Your BMI is ${Math.round(bmi)}, which means you are overweight.`
+    )
+  } else
+    console.log(`Your BMI is ${Math.round(bmi)}, which means you are obese.`)
 }
 
-bmiCalculator(`Dan`, 67, 1.7)
+//Manual Testing
+bmiCalculator(`Subject1`, 45, 1.7) // If value is less than 18.5. Underweight.
+bmiCalculator(`Subject2`, 55, 1.7) // If value is greater than or equal to 18.5 and less than 24.9. Normal weight.
+bmiCalculator(`Subject3`, 75, 1.7) // If value is greater than or equal to 24.9 and less than 30. Overweight.
+bmiCalculator(`Subject4`, 90, 1.7) // If Value is greater than 30. Obese.
